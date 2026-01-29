8022a0953783ed6facdf5c3e56878b482ce2d18f
WastelandX <chessachess42@gmail.com>
2026-01-29 23:22:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def print_banner():
   ▓██ ░▒░█  ██      ░ ░▒        ░ ░  ░  ░▒ ░ ▒░ ▒ ░                  ▒ ░
   ▒ ▒ ░░   ░ ░░  ░░░░
 """
-    console.print(Text(banner_text, style="bold cyan"))
+    console.print(Text(banner_text, style="bold green"))
     console.print(Panel("[bold green]|XyperiaAI| ~ Intelligence Optimized(speed depends on user free ram.) • Uncensored • 1.7B Core[/]\n[bold white]Author: [Act|WastelandX][/]", border_style="cyan", expand=False))
 
 def auto_download():
